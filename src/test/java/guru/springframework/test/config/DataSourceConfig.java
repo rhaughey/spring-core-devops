4617665c17c43add567a1d212b5b879f51c91378
rhaughey <rfhaughey@gmail.com>
2019-06-24 17:38:58 -0400

    Spring profile updates

---
+++ src/test/java/guru/springframework/test/config/DataSourceConfig.java
@@ -0,0 +1,12 @@
+package guru.springframework.test.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by rfh on 6/24/2019
+ */
+@Configuration
+@ComponentScan("guru.springframework.test.ds")
+public class DataSourceConfig {
+}
